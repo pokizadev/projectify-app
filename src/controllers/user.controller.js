4a06279fc5dc509589c7a160735fc27c1489100d
Pokiza Sdv <pokizasdv@gmail.com>
2023-10-31 20:50:17 -0400

    Handle Global Error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { userService } from "../services/user.services.js";
 import jwt from "jsonwebtoken";
-import { catchAsync } from "../utils/catchAsync.js";
+import { catchAsync } from "../utils/catch-async.js";
 
 class UserController {
     signUp = catchAsync(async (req, res) => {
@@ -13,36 +13,24 @@ class UserController {
             lastName: body.lastName,
             password: body.password
         };
-            await userService.signUp(input);
-            res.status(201).json({
-                message: "Success"
-            });
-        
+        await userService.signUp(input);
+        res.status(201).json({
+            message: "Success"
+        });
     });
 
-    login = async (req, res) => {
+    login = catchAsync(async (req, res) => {
         const { body } = req;
         const input = {
             email: body.email,
             password: body.password
         };
-
-        try {
-            const jwt = await userService.login(input);
-            res.status(200).json({
-                token: jwt
-            });
-        } catch (error) {
-            let statusCode = 500;
-            if (error.message === "Invalid Credentials") {
-                statusCode = 401;
-            }
-            res.status(statusCode).json({
-                error: error.message
-            });
-        }
-    };
-    activate = async (req, res) => {
+        const jwt = await userService.login(input);
+        res.status(200).json({
+            token: jwt
+        });
+    });
+    activate = catchAsync(async (req, res) => {
         const {
             query: { activationToken }
         } = req;
@@ -54,39 +42,25 @@ class UserController {
 
             return;
         }
+        await userService.activate(activationToken);
 
-        try {
-            await userService.activate(activationToken);
+        res.status(200).json({
+            message: "Success"
+        });
+    });
 
-            res.status(200).json({
-                message: "Success"
-            });
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({
-                message: error.message
-            });
-        }
-    };
-
-    forgotPassword = async (req, res) => {
+    forgotPassword = catchAsync(async (req, res) => {
         const {
             body: { email }
         } = req;
 
-        try {
-            await userService.forgotPassword(email);
-            res.status(200).json({
-                message: "Password reset email has been sent"
-            });
-        } catch (error) {
-            res.status(500).json({
-                message: error.message
-            });
-        }
-    };
+        await userService.forgotPassword(email);
+        res.status(200).json({
+            message: "Password reset email has been sent"
+        });
+    });
 
-    resetPassword = async (req, res) => {
+    resetPassword = catchAsync(async (req, res) => {
         const {
             body: { password, passwordConfirm },
             headers
@@ -116,45 +90,28 @@ class UserController {
             });
         }
 
-        try {
-            await userService.resetPassword(token, password);
-            res.status(200).json({
-                message: "Password successfully updated"
-            });
-        } catch (error) {
-            res.status(500).json({
-                message: error.message
-            });
-        }
-    };
-    getMe = async (req, res) => {
+        await userService.resetPassword(token, password);
+        res.status(200).json({
+            message: "Password successfully updated"
+        });
+    });
+    getMe = catchAsync(async (req, res) => {
         const { userId } = req;
-        try {
-            const me = await userService.getMe(userId);
 
-            res.status(200).json({
-                data: me
-            });
-        } catch (error) {
-            res.status(500).json({
-                message: error.message
-            });
-        }
-    };
+        const me = await userService.getMe(userId);
 
-    logout = async (req, res) => {
-        try {
-            res.status(200).send({
-                token: ""
-            });
-        } catch (error) {
-            res.status(500).json({
-                message: error.message
-            });
-        }
-    };
+        res.status(200).json({
+            data: me
+        });
+    });
 
-    createTask = async (req, res) => {
+    logout = catchAsync(async (req, res) => {
+        res.status(200).send({
+            token: ""
+        });
+    });
+
+    createTask = catchAsync(async (req, res) => {
         const { userId, body } = req;
 
         const input = {
@@ -170,48 +127,25 @@ class UserController {
             return;
         }
 
-        try {
-            const data = await userService.createTask(userId, input);
-            res.status(201).json({
-                data
-            });
-        } catch (error) {
-            res.status(500).json({
-                message: error.message
-            });
-        }
-    };
+        const data = await userService.createTask(userId, input);
+        res.status(201).json({
+            data
+        });
+    });
 
-    getTasks = async (req, res) => {
+    getTasks = catchAsync(async (req, res) => {
         const { userId } = req;
+        const tasks = await userService.getTasks(userId);
+        res.status(200).json({ data: tasks });
+    });
 
-        try {
-            const tasks = await userService.getTasks(userId);
-            res.status(200).json({ data: tasks });
-        } catch (error) {
-            res.status(500).json({
-                message: error.message
-            });
-        }
-    };
-
-    getTask = async (req, res) => {
+    getTask = catchAsync(async (req, res) => {
         const { userId, params } = req;
-        try {
-            const task = await userService.getTask(userId, params.taskId);
-            res.status(200).json({ data: task });
-        } catch (error) {
-            let status = 500;
-            if (error.message === "Task not fount") {
-                status = 404;
-            }
-            res.status(status).json({
-                message: error.message
-            });
-        }
-    };
+        const task = await userService.getTask(userId, params.taskId);
+        res.status(200).json({ data: task });
+    });
 
-    updateTask = async (req, res) => {
+    updateTask = catchAsync(async (req, res) => {
         const { userId, params, body } = req;
 
         const input = {};
@@ -231,37 +165,16 @@ class UserController {
             return;
         }
 
-        try {
-            await userService.updateTask(userId, params.taskId, input);
-            res.status(204).send();
-        } catch (error) {
-            let status = 500;
-            if (error.message === "Task not found") {
-                status = 404;
-            }
-            res.status(status).json({
-                message: error.message
-            });
-        }
-    };
+        await userService.updateTask(userId, params.taskId, input);
+        res.status(204).send();
+    });
 
-    deleteTask = async (req, res) => {
+    deleteTask = catchAsync(async (req, res) => {
         const { userId, params } = req;
 
-        try {
-            await userService.deleteTask(userId, params.taskId);
-            res.status(204).send();
-        } catch (error) {
-            let status = 500;
-            if (error.message === "Task not found") {
-                status = 404;
-            }
-
-            res.status(status).json({
-                message: error.message
-            });
-        }
-    };
+        await userService.deleteTask(userId, params.taskId);
+        res.status(204).send();
+    });
 }
 
 export const userController = new UserController();
